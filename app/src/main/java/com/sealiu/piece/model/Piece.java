6e8a312c375d735f729aa81255f79e824c26647b
Peach <568797992@qq.com>
2016-07-02 09:48:56 +0800

    实体类创建 by:Peach

--- app/src/main/java/com/sealiu/piece/model/Piece.java
+++ app/src/main/java/com/sealiu/piece/model/Piece.java
@@ -1,8 +1,122 @@
 package com.sealiu.piece.model;
 
+import java.util.Date;
+
 /**
  * Created by liuyang
  * on 2016/7/1.
  */
 public class Piece {
+    private Integer id;
+    //作者(外键)
+    private Integer authorID;
+    //内容
+    private String content;
+    private String image;
+    //视频
+    private String video;
+    //piece类型
+    private Integer type;
+    //经度
+    private Double longitude;
+    //维度
+    private Double latitude;
+    //创建时间
+    private Date createdTime;
+    //浏览次数
+    private Double viewCount;
+//    //点赞功能
+//    private Double clickLike;
+    //可见性
+    private Boolean visibility;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAuthorID() {
+        return authorID;
+    }
+
+    public void setAuthorID(Integer authorID) {
+        this.authorID = authorID;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getVideo() {
+        return video;
+    }
+
+    public void setVideo(String video) {
+        this.video = video;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public Double getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(Double viewCount) {
+        this.viewCount = viewCount;
+    }
+
+    public Boolean getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(Boolean visibility) {
+        this.visibility = visibility;
+    }
+
+
 }
--- app/src/main/java/com/sealiu/piece/model/User.java
+++ app/src/main/java/com/sealiu/piece/model/User.java
@@ -1,9 +1,119 @@
 package com.sealiu.piece.model;
 
+import java.util.Date;
+
 /**
  * Created by liuyang
  * on 2016/7/1.
  */
 public class User {
+    private Integer id;
+    //昵称
+    private String name;
+    //邮箱
+    private String email;
+    //手机号
+    private Integer phone;
+    //密码
+    private String password;
+    //第三方登录qq返回值
+    private String qq;
+    //第三方登录微信返回值
+    private String wechat;
+    //头像
+    private String picture;
+    //个人简介
+    private String bio;
+    //用户类型
+    private Integer type;
+    //注册时间
+    private Date regTime;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq;
+    }
+
+    public String getWechat() {
+        return wechat;
+    }
+
+    public void setWechat(String wechat) {
+        this.wechat = wechat;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Date getRegTime() {
+        return regTime;
+    }
+
+    public void setRegTime(Date regTime) {
+        this.regTime = regTime;
+    }
 }
