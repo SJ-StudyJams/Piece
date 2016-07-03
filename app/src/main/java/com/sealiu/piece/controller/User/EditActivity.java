07bec40ce50a658d0407afaeac0b477853bcfb07
sealiu <sealiu0217@gmail.com>
2016-07-03 16:43:21 +0800

    个人信息加返回按钮，监听点击事件。

--- app/src/main/java/com/sealiu/piece/controller/User/EditActivity.java
+++ app/src/main/java/com/sealiu/piece/controller/User/EditActivity.java
@@ -1,8 +1,10 @@
 package com.sealiu.piece.controller.User;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.sealiu.piece.R;
 
@@ -15,7 +17,43 @@ public class EditActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.edit_user_info);
+
+        findViewById(R.id.user_name).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "username", Snackbar.LENGTH_SHORT).setAction("Action", null)
+                        .show();
+            }
+        });
+
+        findViewById(R.id.user_bio).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "bio", Snackbar.LENGTH_SHORT).setAction("Action", null)
+                        .show();
+            }
+        });
+
+
+        findViewById(R.id.user_birth).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "birth", Snackbar.LENGTH_SHORT).setAction("Action", null)
+                        .show();
+            }
+        });
+
+        findViewById(R.id.head_picture).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "headPicture", Snackbar.LENGTH_SHORT).setAction("Action", null)
+                        .show();
+            }
+        });
+
+
     }
 }
