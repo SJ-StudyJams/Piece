ebeabcf0e00c904c638ab1ceb260b6b5e89d7c97
art2cat <yiming.whz@gmail.com>
2016-07-03 20:18:16 +0800

    增加DatePickerDialog设置生日

--- app/src/main/java/com/sealiu/piece/controller/User/EditActivity.java
+++ app/src/main/java/com/sealiu/piece/controller/User/EditActivity.java
@@ -1,10 +1,17 @@
 package com.sealiu.piece.controller.User;
 
+import android.annotation.TargetApi;
+import android.app.DatePickerDialog;
+import android.icu.util.Calendar;
+import android.icu.util.ChineseCalendar;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.sealiu.piece.R;
 
@@ -39,12 +46,25 @@ public class EditActivity extends AppCompatActivity {
 
 
         findViewById(R.id.user_birth).setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.N)
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "birth", Snackbar.LENGTH_SHORT).setAction("Action", null)
                         .show();
+                final Calendar calendar = Calendar.getInstance();
+                new DatePickerDialog(EditActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int monthOfYear,
+                                          int dayOfMonth) {
+                        EditText setBirth = (EditText) findViewById(R.id.user_birth);
+                        String birth = year + "-" + monthOfYear
+                                + "-" + dayOfMonth;
+                        setBirth.setText(birth);
+                    }
+                }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar
+                            .get(Calendar.DAY_OF_MONTH)).show();
             }
-        });
+         });
 
         findViewById(R.id.head_picture).setOnClickListener(new View.OnClickListener() {
             @Override
