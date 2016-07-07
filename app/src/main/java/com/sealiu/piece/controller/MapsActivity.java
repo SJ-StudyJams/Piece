bef7e33a57ba0c68507c5d8b218e91d74e722148
SEALiu <sealiu0217@gmail.com>
2016-07-07 20:17:22 +0800

    获取接收纸条类型的设置值示例（Set<String>）

--- app/src/main/java/com/sealiu/piece/controller/MapsActivity.java
+++ app/src/main/java/com/sealiu/piece/controller/MapsActivity.java
@@ -2,13 +2,16 @@ package com.sealiu.piece.controller;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -23,6 +26,8 @@ import com.sealiu.piece.controller.LoginRegister.LoginActivity;
 import com.sealiu.piece.controller.User.UserActivity;
 import com.sealiu.piece.model.Constants;
 import com.sealiu.piece.utils.SPUtils;
+
+import java.util.Set;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -43,6 +48,17 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // 获取 preferenceSettings 中的值，示例：
+        SharedPreferences SP = PreferenceManager.getDefaultSharedPreferences(this);
+
+        Set<String> pref_piece_nearby_key = SP.getStringSet("pref_piece_nearby_key", null);
+        if (pref_piece_nearby_key != null)
+            Log.i("MapsActivity", "允许接收小纸条类型：" + pref_piece_nearby_key.toString());
+    }
 
     /**
      * Manipulates the map once available.
