a9cf224c1f4f2c7ba2c792e03871d0d048ea43ac
art2cat <yiming.whz@gmail.com>
2016-07-15 17:11:47 +0800

    添加PieceMainService

---
+++ app/src/main/java/com/sealiu/piece/service/PieceMainService.java
@@ -0,0 +1,32 @@
+package com.sealiu.piece.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by art2cat on 16-7-15.
+ */
+public class PieceMainService extends Service {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
